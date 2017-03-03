ce3e87cd28d48fbff6fbb2d500236f56b765eb0d
vferman2000 <vferman2000@gmail.com>
2017-03-02 22:25:19 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,287 @@
+/* case  notation conversion
+
+var someMonth;
+function theMonth 	 // function to return current month
+currentMonth  	 // a constant
+var summerMonth; 		// an array of summer months
+function myLibrary 	    // a function 
+MyLibraryaFunction; 		// a function 
+*/
+
+
+/* numerical literal expression
+42
+*/
+
+/*string literal expression
+"Hi!"
+
+*/
+
+/*Boolean literal expression
+true
+
+*/
+
+/*null literal expression
+null
+
+*/
+
+/*example of complex/variable expressions
+
+var anExpression = 5 / (6 * 8);
+
+*/
+
+/*example of complex/variable expressions
+
+var anotherExpression = 15 % (4 + 7)
+
+*/
+
+/* Declare 9 variables for the following identifiers
+
+var firstName;
+var lastName;
+var sAddress;
+var sCity;
+var sState;
+var zipCode;
+var yourAge;
+var referralSource;
+var bMayWeContactYou;
+
+*/
+
+/* method 1 for declaring and assigning values
+
+var firstName;
+var lastName;
+var sAddress;
+var sCity;
+var sState;
+var zipCode;
+var yourAge;
+var referralSource;
+var bMayWeContactYou;
+
+firstName = "Veronica";
+lastName = "Smith";
+sAddress = "2990 Rock St.";
+sCity = "San Diego";
+sState = "California";
+zipCode = "91916";
+yourAge = "13";
+referralSource = "newspaper";
+bMayWeContactYou = false;
+*/
+
+
+
+/* method 2 for declaring and assigning values
+
+var firstName = "Veronica";
+var lastName = "Smith";
+var sAddress = "2990 Rock St.";
+var sCity = "San Diego";
+var sState = "California";
+var zipCode = "91916";
+var yourAge = "13";
+var referralSource = "newspaper";
+var bMayWeContactYou = false;
+
+*/
+
+
+
+/* method 3 for declaring and assigning values
+
+var firstName = "Veronica", lastName = "Smith", sAddress = "2990 Rock St.",
+sCity = "San Diego", sState = "California", zipCode = "91916", yourAge = "13",
+referralSource = "newspaper", bMayWeContactYou = false;
+*/
+
+/*add a number and a string
+var yearsDriving = 5;
+console.log("Tom has been driving for " + yearsDriving + " years.");
+*/
+
+/*add a Boolean and a string
+var isValid = true;
+console.log("Tom is a student" + " " + isValid);
+*/
+
+/* add a number and a Boolean
+var x = 5;
+var b = false;
+console.log(x + b);
+*/
+
+
+/* add a number and a Boolean
+var b = true;
+var x = 10;
+console.log(b + x);
+*/
+
+/* program that returns undefined
+
+var month = "January";
+console.log(month + year);
+var year = "2001";
+*/
+
+/* first example is not valid. This is how you fix it.
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+/* Create a variable that produces a null value in the console window. 
+
+var y = null;
+console.log(y);
+
+*/
+
+/* create a variable that produces an undefined value in the console window.
+
+var y;
+console.log(y);
+*/
+
+/* Use the typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+
+console.log(typeof "February" + "\n" + typeof 5 + "\n" + typeof true + "\n" + typeof null + "\n" + typeof sMonth);
+*/
+
+/* display text in the alert box
+
+alert("Hello" + " " + "Veronica Ferman" + ", welcome to the Javascript class!");
+
+*/
+
+/* use Name variable
+
+var name = "Veronica Ferman";
+
+alert("Hello" + " " + name + ", welcome to the JavaScript class!");
+*/
+
+
+
+/* variable called course
+
+var name = "Veronica Ferman";
+var course = "JavaScript";
+
+alert("Hello" + " " + name + ", welcome to the" + " " + course + "class!");
+
+*/
+
+
+/* line break
+var name = "Veronica Ferman";
+var course = "JavaScript";
+
+alert("Hello" + " " + name + "." + "\n" + "Welcome to the" + " " + course + " " + "class!");
+*/
+
+
+/* prompt that asks the user for their name
+
+
+var name = prompt("what is your name?");
+var course = "JavaScript";
+
+alert("Hello" + " " + name + "," + "\n" + "welcome to the" + " " + course + "class!");
+
+*/
+
+/* prompt that asks the user for course they are taking
+
+var name = prompt("what is your name?");
+var course = prompt("what couse are you taking?");
+
+alert("Hello" + " " + name + "," + "\n" + "welcome to the" + " " + course + " " + "class!");
+
+*/
+
+/* sum of two numbers
+
+var x = 10;
+var y = 20;
+
+console.log(x + y);
+*/
+
+/* add and assign
+
+var x = 20;
+x += 20;
+
+console.log(x);
+
+*/
+
+
+/* multiply and assign
+
+var x = 20;
+x *=5;
+
+console.log(x);
+
+*/
+
+
+/* the modolus operator
+
+var x= 20 % 3;
+x /=1;
+ 
+console.log(x);
+*/
+
+
+/* application that evaluates to true
+
+var x = 5;
+var y = 5;
+
+console.log(x==y);
+*/
+
+
+/* application that evaluates to false
+
+var y = 25;
+var x = 25;
+
+console.log(x !== 25);
+
+*/
+
+/* new operator 
+
+var widget = new Object();
+
+console.log (typeof widget);
+
+*/
+
+/* instanceof opeator true 
+
+var widget = new Object();
+
+console.log (widget instanceof Object);
+*/
+
+
+/* instanceof operator false
+
+var widget = new Object();
+
+console.log (widget instanceof String);
+*/
